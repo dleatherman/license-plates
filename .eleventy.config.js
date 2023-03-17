75ac0c27a216a8c0030729c300331159c30646b8
Dan Leatherman <dleatherman@gmail.com>
2023-03-17 14:46:36 -0400

    webc

--- .eleventy.config.js
+++ .eleventy.config.js
@@ -1,5 +1,7 @@
+const pluginWebc = require("@11ty/eleventy-plugin-webc");
 
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addPlugin(pluginWebc);
 
   return {
     passthroughFileCopy: true,
