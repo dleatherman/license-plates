5af8ce919b903a0ce22a2d8a5a200235cd47ab4e
Dan Leatherman <dleatherman@gmail.com>
2023-03-22 20:38:36 -0400

    list states in render function

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,8 +2,10 @@ const pluginWebc = require("@11ty/eleventy-plugin-webc");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(pluginWebc, {
-    components: "components/**/*.webc"
+    components: "./components/**/*.webc"
   });
+
+  eleventyConfig.addPassthroughCopy('./src/style.css')
 
   return {
     dir: {
